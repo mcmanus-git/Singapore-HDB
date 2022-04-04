8ede43fedb6cbc7fd92c978eff1306c258f1645a
Stuart Ong <onstuart@gmail.com>
2022-04-04 10:11:32 +0800

    Updated resale_price_norm for latest Q with no HPI

--- michael/SQL/resale_price_norm_vw.sql
+++ michael/SQL/resale_price_norm_vw.sql
@@ -1,9 +1,9 @@
 select rp.*,
-       (rp.resale_price / rpi.rebased_to_latest * 100) as resale_price_norm,
-       (rp.price_per_sq_ft / rpi.rebased_to_latest * 100) as price_per_sq_ft_norm,
-       (rp.price_per_sq_ft_per_lease_yr / rpi.rebased_to_latest * 100) as price_per_sq_ft_per_lease_yr_norm,
-       (rp.price_per_sq_m / rpi.rebased_to_latest * 100) as price_per_sq_m_norm,
-       (rp.price_per_sq_m_per_lease_yr / rpi.rebased_to_latest * 100) as price_per_sq_m_per_lease_yr_norm
+       coalesce((rp.resale_price / rpi.rebased_to_latest * 100),rp.resale_price) as resale_price_norm,
+       coalesce((rp.price_per_sq_ft / rpi.rebased_to_latest * 100),rp.price_per_sq_ft) as price_per_sq_ft_norm,
+       coalesce((rp.price_per_sq_ft_per_lease_yr / rpi.rebased_to_latest * 100),rp.price_per_sq_ft_per_lease_yr) as price_per_sq_ft_per_lease_yr_norm,
+       coalesce((rp.price_per_sq_m / rpi.rebased_to_latest * 100), rp.price_per_sq_m) as price_per_sq_m_norm,
+       coalesce((rp.price_per_sq_m_per_lease_yr / rpi.rebased_to_latest * 100),rp.price_per_sq_m_per_lease_yr) as price_per_sq_m_per_lease_yr_norm
 from resale_prices_full rp
          left outer join resale_price_index_rebased rpi
                          on extract(quarter from rp.month) = right(rpi.quarter, 1)::int8
