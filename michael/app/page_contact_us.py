772fe37ece8769f38a0b3ac4f249a525dad102d4
Michael M <melancholic_maclean@yahoo.com>
2022-04-18 22:09:21 -0400

    updated format on contact page

--- michael/app/page_contact_us.py
+++ michael/app/page_contact_us.py
@@ -7,18 +7,27 @@ discloser, footer = create_footer()
 header = html.H1('Contact Us')
 
 
-bio_text = \
+line_break_text = """___  """
+
+bio_text_tom = \
 """
-___  
+
 ## Tom Bresee  
 
 [LinkedIn](https://www.linkedin.com/in/tombresee/) | [GitHub](https://github.com/tombresee) | tbresee@umich.edu
-___  
+"""
+
+bio_text_michael = \
+"""
+
 ## Michael McManus  
 
 [LinkedIn](https://www.linkedin.com/in/michael-mcmanus/) | [GitHub](https://github.com/mcmanus-git) | [Medium](https://medium.com/@mcmanus_data_works) | msmcmanu@umich.edu  
 
-___  
+"""
+bio_text_stuart = \
+"""
+
 ## Stuart Ong  
 
 [LinkedIn](#) | [GitHub](#)    
@@ -29,7 +38,18 @@ def create_page_contact_us():
     layout = html.Div([
         nav,
         html.Div([header], style={'margin': '5% 10% 5% 10%', 'textAlign': 'center'}),
-        html.Div([dcc.Markdown(bio_text)], style={'margin': '5% 10% 5% 10%'}),
+        html.Div([dcc.Markdown(line_break_text)], style={'margin': '5% 10% 5% 10%'}),
+        html.Br(),
+        html.Div([dcc.Markdown(bio_text_tom)], style={'margin': '5% 10% 5% 10%'}),
+        html.Br(),
+        html.Div([dcc.Markdown(line_break_text)], style={'margin': '5% 10% 5% 10%'}),
+        html.Br(),
+        html.Div([dcc.Markdown(bio_text_michael)], style={'margin': '5% 10% 5% 10%'}),
+        html.Br(),
+        html.Div([dcc.Markdown(line_break_text)], style={'margin': '5% 10% 5% 10%'}),
+        html.Br(),
+        html.Div([dcc.Markdown(bio_text_stuart)], style={'margin': '5% 10% 5% 10%'}),
+        html.Br(),
         footer
     ])
     return layout
