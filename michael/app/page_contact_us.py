59037f8932449d80144d45a4ef2d09b36d8c0e61
Michael M <melancholic_maclean@yahoo.com>
2022-04-18 23:09:33 -0400

    added stuart links and changed formatting

--- michael/app/page_contact_us.py
+++ michael/app/page_contact_us.py
@@ -21,16 +21,21 @@ bio_text_michael = \
 """
 
 ## Michael McManus  
-
-[LinkedIn](https://www.linkedin.com/in/michael-mcmanus/) | [GitHub](https://github.com/mcmanus-git) | [Medium](https://medium.com/@mcmanus_data_works) | msmcmanu@umich.edu  
+Michael is a Master's Student at the University of Michigan School of Information pursuing his degree in Applied Data Science.  
+He works as a Senior Data Analyst at Consumers Energy and loves all things data.    
 
 """
+
+contacts_michael = """[LinkedIn](https://www.linkedin.com/in/michael-mcmanus/) | [GitHub](https://github.com/mcmanus-git) | [Medium](https://medium.com/@mcmanus_data_works) | msmcmanu@umich.edu"""
+
 bio_text_stuart = \
 """
 
 ## Stuart Ong  
 
-[LinkedIn](#) | [GitHub](#)    
+[LinkedIn](https://www.linkedin.com/in/stuart-ong) | [GitHub](https://github.com/stuartong)    
+
+
 
 """
 
@@ -39,17 +44,16 @@ def create_page_contact_us():
         nav,
         html.Div([header], style={'margin': '5% 10% 5% 10%', 'textAlign': 'center'}),
         html.Div([dcc.Markdown(line_break_text)], style={'margin': '5% 10% 5% 10%'}),
-        html.Br(),
         html.Div([dcc.Markdown(bio_text_tom)], style={'margin': '5% 10% 5% 10%'}),
         html.Br(),
         html.Div([dcc.Markdown(line_break_text)], style={'margin': '5% 10% 5% 10%'}),
+        html.Div([dcc.Markdown(bio_text_michael)], style={'margin': '5% 10% 0% 10%'}),
         html.Br(),
-        html.Div([dcc.Markdown(bio_text_michael)], style={'margin': '5% 10% 5% 10%'}),
-        html.Br(),
+        html.Div([dcc.Markdown(contacts_michael)], style={'margin': '0% 10% 0% 10%'}),
         html.Div([dcc.Markdown(line_break_text)], style={'margin': '5% 10% 5% 10%'}),
-        html.Br(),
         html.Div([dcc.Markdown(bio_text_stuart)], style={'margin': '5% 10% 5% 10%'}),
         html.Br(),
+        html.Div([dcc.Markdown(line_break_text)], style={'margin': '5% 10% 5% 10%'}),
         footer
     ])
     return layout
