605b4a46d2fd27a1dd5ac38fe77b84a8a1628237
Michael M <melancholic_maclean@yahoo.com>
2022-04-21 01:54:08 -0400

    troubleshooting

--- michael/app/app.py
+++ michael/app/app.py
@@ -7,7 +7,7 @@ import dash_bootstrap_components as dbc
 # SIMPLEX, SKETCHY, SLATE, SOLAR, SPACELAB, SUPERHERO, UNITED, YETI, ZEPHYR.
 # https://dash-bootstrap-components.opensource.faculty.ai/docs/themes/
 
-app = dash.Dash(__name__,
+application = app = dash.Dash(__name__,
                 suppress_callback_exceptions=True,
                 external_stylesheets=[dbc.themes.LUX,
                                       'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'])
